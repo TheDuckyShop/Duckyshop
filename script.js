d0d6208ef6d9d7992c1b7ea7b70a77950bf66124
hei1810 <hei180.0811@gmail.com>
2024-12-03 09:02:01 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("pockets").addEventListener("click", function() {
+    window.location.href = "pockets.html";
+});
+
+document.getElementById("schriften").addEventListener("click", function() {
+    window.location.href = "schriften.html";
+});
+
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
